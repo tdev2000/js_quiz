f253c2b899f7abb22bc85f326a5445adec72981f
tdev2000 <thaveeshadevineew@gmail.com>
2024-05-24 14:50:06 +0100

    quiz2:server created

---
+++ quiz2/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+//Use cors middleware
+const path = require("path");
+var randomstring = require("randomstring");
+
+
+const PORT = 3000;
+
+//API call - http://localhost:3000/
+app.get("/", (req, res) => {
+    console.log("api call received !");
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
