72343a014c611e5a7b396c8ba2a0e51293b86646
tdev2000 <thaveeshadevineew@gmail.com>
2024-05-24 15:26:15 +0100

    quiz2:Server

--- quiz2/server.js
+++ quiz2/server.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// app listen
 
+
+// app listen
+app.listen(PORT, () => {
+    console.log("App is running on : " + PORT);
+})
 
