ebf723d2d788656e73065d2cc564ae09ee647232
tdev2000 <thaveeshadevineew@gmail.com>
2024-05-24 13:45:42 +0100

    Script updated

--- quiz1/script.js
+++ quiz1/script.js
@@ -1,1 +1,14 @@
-console.log("Hello!")
+let people = [
+  { name: "John", age: 21, city: "New York" },
+  { name: "Mike", age: 28, city: "Moscow" },
+  { name: "Danny", age: 30, city: "London" },
+  { name: "Lisa", age: 26, city: "Paris" },
+  { name: "Sophie", age: 19, city: "Berlin" },
+];
+
+let newArray = [];
+for (let index = 0; index < people.length; index++) {
+  newArray.push(people[index].name);
+}
+
+console.log("This is the new array: " + newArray);
